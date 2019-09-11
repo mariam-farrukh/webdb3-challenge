e0f24b2f8f6d7fb46743168f466dc757dcbb81d1
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-11 16:13:54 -0500

    MVP Completed: Finished Database Methods

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,12 @@ const db = require('../data/db-config.js');
 
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps,
+    add,
+    addStep,
+    update,
+    remove
 };
 
 function find() {
@@ -11,4 +16,38 @@ function find() {
 
 function findById(id) {
     return db("schemes").where({ id });
+}
+
+function findSteps(id) {
+    return db("schemes as s")
+      .innerJoin("steps as stp", "s.id", "stp.scheme_id")
+      .select("s.scheme_name", "stp.step_number", "stp.instructions")
+      .where({ scheme_id: id })
+      .orderBy("stp.step_number")
+      .then(steps => {
+          return steps;
+      })
+}
+
+function add(scheme) {
+    return db('schemes').insert(scheme);
+}
+
+function addStep(stepData, id) {
+    return db("steps as stp")
+        .innerJoin("schemes as s", "stp.scheme_id", "=", "s.id")
+        .where({ scheme_id: id })
+        .insert(stepData);
+}
+
+function update(changes, id) {
+    return db("schemes")
+        .where({ id })
+        .update(changes);
+}
+
+function remove(id) {
+    return db("schemes")
+        .where({ id })
+        .del();
 }
