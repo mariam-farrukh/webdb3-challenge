32be55685f4bfd7bcd7c8a50214234ea90d54ce3
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-11 15:50:11 -0500

    Set up files, completed find and findby steps

---
+++ schemes/scheme-model.js
@@ -0,0 +1,14 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    find,
+    findById
+};
+
+function find() {
+    return db("schemes");
+}
+
+function findById(id) {
+    return db("schemes").where({ id });
+}
